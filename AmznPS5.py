bd82af898bf6846da37a2cc93cb511f90b15bcbc
Vishu Singh <visingh.in@gmail.com>
2021-05-14 12:23:09 +0200

    Amzn.in ps5

---
+++ AmznPS5.py
@@ -0,0 +1,141 @@
+#%%
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+import urllib.request
+import os
+from configparser import ConfigParser
+import tkinter as tk
+
+class BBYbot():
+  def __init__(self,config):
+
+    self.driver = webdriver.Chrome('./chromedriver')
+    self.url="https://www.amazon.in/"
+    self.username=config.get('BBY_ACCOUNT','USERNAME')
+    self.password=config.get('BBY_ACCOUNT','PASSWORD')
+    self.ID=config.get('BBY_ACCOUNT','ID')
+    self.card=config.get('CARD_INFO','CARD#')
+    self.cardsecurity=config.get('CARD_INFO','CARDSECURITY')
+    self.expm=config.get('CARD_INFO','EXPM')
+    self.expy=config.get('CARD_INFO','EXPY')
+  
+
+    self.driver.get(self.url)
+
+  def Login(self):
+    # time.sleep(5)
+    # self.driver.find_element_by_class_name("nav-progressive-attribute").click()
+    self.driver.find_element_by_css_selector("a#nav-link-accountList").click()
+    time.sleep(.5)
+    # self.driver.find_element_by_class_name("btn-secondary").click()
+    time.sleep(2)
+    username_input=self.driver.find_element_by_xpath('//*[@id="ap_email"]')
+    username_input.send_keys(self.username)
+    self.driver.find_element_by_class_name("a-button-input").click()
+    password_input = self.driver.find_element_by_xpath('//*[@id="ap_password"]')
+    password_input.send_keys(self.password)
+    password_input.submit()
+    time.sleep(3)
+
+    try:
+      employee=self.driver.find_element_by_xpath('/html/body/div[1]/div/section/main/div[1]/div/div/div/div/form/div[1]/div/input')
+      employee.send_keys(self.ID)
+      employee.submit()
+    except:
+      return
+
+  def searchtag(self, search_tag):
+    self.driver.get('https://www.amazon.in/gp/product/B08FV5GC28/ref=s9_acss_bw_cg_button_2a1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-1&pf_rd_r=X3DVWGP1TW3DA5SVRY7M&pf_rd_t=101&pf_rd_p=01e9c541-42d0-4ab7-ba81-f69fbb14851b&pf_rd_i=21725163031')
+    # searchbar=self.driver.find_element_by_id('twotabsearchtextbox')
+    # searchbar.send_keys(search_tag)
+    # searchbar.submit()
+
+  def in_stock(self):
+    time.sleep(5)
+    try:
+      
+      item = self.driver.find_element_by_id('submit.add-to-cart-announce')
+      #webdriver.ActionChains(self.driver).click_and_hold(self.driver.find_elements_by_class_name('btn btn-primary btn-lg btn-block btn-leading-ficon add-to-cart-button')).perform()
+      #webdriver.ActionChains(self.driver).release().preform()
+      print("In stock!")
+      return True
+      
+
+    except:
+      print("Item is out of stock")
+      return False
+
+  def add_toCart(self, incart):
+    try:
+      time.sleep(3)
+      item =self.driver.find_element_by_id('submit.add-to-cart-announce')
+      item.click()
+      time.sleep(5)							
+      go_to_cart_button= self.driver.find_element_by_id("nav-cart")
+      
+      go_to_cart_button.click()
+      incart=True
+      return incart
+      
+    except:
+      print("Couldnt add to cart trying again")
+      incart=False
+  def checkout(self):
+    time.sleep(3)
+    #selects shipping
+    self.driver.find_element_by_xpath('/html/body/div[1]/main/div/div[2]/div[1]/div/div/span/div/div[1]/div[1]/section[1]/div[4]/ul/li/section/div[2]/div[2]/form/div[2]/fieldset/div[2]/div[1]/div/div/div/input').click()
+    #presses checkout
+    self.driver.find_element_by_xpath('/html/body/div[1]/main/div/div[2]/div[1]/div/div/span/div/div[1]/div[1]/section[2]/div/div/div[3]/div/div[1]/button').click()
+    #continues to payment
+    time.sleep(2)
+    self.driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div[1]/div[1]/main/div[2]/div[2]/form/section/div/div[2]/div/div/button').click()
+    time.sleep(4)
+    paymentinfo=self.driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div[1]/div[1]/main/div[2]/div[3]/div/section/div[1]/div/section/div[1]/div/input')
+    paymentinfo.send_keys(self.card)
+    selectmm = Select(self.driver.find_element_by_name('expiration-month'))
+    selectmm.select_by_visible_text(self.expm)
+    selectyy = Select(self.driver.find_element_by_name('expiration-year'))
+    selectyy.select_by_visible_text(self.expy)
+    securitycode=self.driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div[1]/div[1]/main/div[2]/div[3]/div/section/div[1]/div/section/div[2]/div[2]/div/div[2]/div/input')
+    securitycode.send_keys(self.cardsecurity)
+    self.driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div[1]/div[1]/main/div[2]/div[3]/div/section/div[4]/button').click()
+
+  def closeEmailprompt(self):
+    self.driver.find_element_by_class_name("c-modal-close-icon").click()
+  def close(self):
+    self.driver.close()
+def getinput():
+  sku_num=skunumber.get()
+  print(sku_num)
+  return sku_num
+
+print("Starting Bot")
+searchtag=''
+config_file= ConfigParser()
+config_file.read("config.ini")
+bot = BBYbot(config_file)
+try:
+  time.sleep(5)
+  bot.Login()
+except:
+  bot.closeEmailprompt()
+  time.sleep(3)
+  bot.Login()
+time.sleep(4)
+bot.searchtag("6429440")
+instock= bot.in_stock()
+incart=False
+if (instock==True):
+  while(incart != True):
+    incart=bot.add_toCart(incart)
+    print("In cart!")
+time.sleep(1)
+#bot.checkout()
+
+print("compiled")
+bot.close()
+
+# %%
